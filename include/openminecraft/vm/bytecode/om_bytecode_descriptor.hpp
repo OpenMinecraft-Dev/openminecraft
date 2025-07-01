cab47c01fe5093c2fcac46d1ca897ec617bcaeb7
MetsukiMio <guoxiuchen20170402@163.com>
2025-07-01 21:31:37 +0800

    VM: restore code

--- include/openminecraft/vm/bytecode/om_bytecode_descriptor.hpp
+++ include/openminecraft/vm/bytecode/om_bytecode_descriptor.hpp
@@ -11,20 +11,9 @@ using namespace openminecraft::util;
 
 namespace openminecraft::vm::bytecode::descriptor
 {
-struct TempNonPrimitiveVariable
-{
-    std::string type;
-};
-struct TempArrayVariable
-{
-    std::string type;
-    int length;
-};
 OMResult<std::string, std::string> decodeType(std::string raw, int *p);
 typedef std::pair<std::vector<std::string>, std::string> methodSig;
 OMResult<methodSig, std::string> decodeSignature(std::string raw, int *p);
-OMResult<std::any, std::string> checkArgCompat(std::any data, std::string type);
-std::any createEmptyData(std::string type);
 } // namespace openminecraft::vm::bytecode::descriptor
 
 #endif
