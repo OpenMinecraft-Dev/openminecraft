c689a1e54401b05ddf8cbc2e57a10b3f94915eda
MetsukiMio <guoxiuchen20170402@163.com>
2025-07-25 15:30:19 +0800

    VM (new pixeltower): Windows lazy 堆内存分配

--- plat/windows/om_mem_plat.cpp
+++ plat/windows/om_mem_plat.cpp
@@ -1,23 +1,46 @@
 #include "openminecraft/mem/om_mem_prealloc.hpp"
 #include "openminecraft/mem/om_mem_record.hpp"
+#include <Windows.h>
+#include <errhandlingapi.h>
 #include <malloc.h>
+#include <memoryapi.h>
+#include <new>
+#include <oleauto.h>
+#include <winnt.h>
 
 namespace openminecraft::mem
 {
 OMHeap::OMHeap(uint64_t minSize, uint64_t maxSize) : logger("OMHeap", this), maxSize(maxSize), minSize(minSize)
 {
+    block = VirtualAlloc(nullptr, maxSize, MEM_RESERVE, PAGE_NOACCESS);
+    if (block == nullptr)
+    {
+        logger.error("[windows] VirtualAlloc fail ({})", GetLastError());
+        throw std::bad_alloc();
+    }
 }
 
 OMHeap::~OMHeap()
 {
+    VirtualFree(block, 0, MEM_RELEASE);
 }
 
 void OMHeap::activate(void *p, uint64_t length)
 {
+    if (!VirtualAlloc(p, length, MEM_COMMIT, PAGE_READWRITE))
+    {
+        logger.error("[windows] VirtualAlloc fail ({})", GetLastError());
+        throw std::bad_alloc();
+    }
 }
 
 void OMHeap::deactivate(void *p, uint64_t length)
 {
+    if (!VirtualFree(p, length, MEM_DECOMMIT))
+    {
+        logger.error("[windows] VirtualFree fail ({})", GetLastError());
+        throw std::bad_alloc();
+    }
 }
 } // namespace openminecraft::mem
 
