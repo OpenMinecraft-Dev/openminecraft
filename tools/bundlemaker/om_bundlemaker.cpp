6010408c118d3a91fcb1e069550ae96330f2bca7
MetsukiMio <guoxiuchen20170402@163.com>
2025-11-29 21:34:55 +0800

    fix getopt on windows

--- tools/bundlemaker/om_bundlemaker.cpp
+++ tools/bundlemaker/om_bundlemaker.cpp
@@ -31,7 +31,21 @@ static void appendFile(std::vector<file_path> &pth, std::string base, std::strin
     }
 }
 
-const char *usagetext = " [options] <file>\nthis tool will read the target file in default\n\n  -r / --read\treads the bundle file\n  -c / --create\tcreate bundle file\n  -P / --push\tonly works in create mode, attach file to new archives\n  -a / --author\tset the name of the author\n  -t / --target\tset the target input/output file\n  -h / --help\tshow this message";
+const char *usagetext = " [options] <file>\n"
+"this tool will read the target file in default\n"
+"\n"
+"  -r / --read\tread the bundle file\n"
+"  -c / --create\tcreate bundle file\n"
+"  -P / --push\tonly works in create mode, attach file to new archives\n"
+"  -a / --author\tonly works in create mode, set the name of the author\n"
+"  -t / --target\tset the target input/output file\n"
+"  -h / --help\tshow this message\n"
+"\n"
+"For example: \n"
+"  -a Cyrene -c -t out.bundle -P hello_world.txt\n"
+"    create a bundle out.bundle with file hello_world.txt (author is Cyrene)\n"
+"  -r -t out.bundle\n"
+"    read the out.bundle";
 
 int main(int argc, char **argv)
 {
@@ -48,17 +62,17 @@ int main(int argc, char **argv)
 
     int opt;
     int option_index = 0;
-    const char *optstring = "h:a:t:P:cr";
+    const char *optstring = "h:a:t:P:c:r";
     static option long_options[] = {
+        {"help", no_argument, nullptr, 'h'},
         {"read", no_argument, nullptr, 'r'},
         {"create", no_argument, nullptr, 'c'},
         {"push", optional_argument, nullptr, 'P'},
         {"author", optional_argument, nullptr, 'a'},
         {"target", optional_argument, nullptr, 't'},
-        {"help", no_argument, nullptr, 'h'},
         {nullptr, 0, nullptr, 0}
     };
-    while ((opt = getopt_long_only(argc, argv, optstring, long_options, &option_index)) != -1) {
+    while ((opt = getopt_long(argc, argv, optstring, long_options, &option_index)) != -1) {
         switch (opt)
         {
         case 'h':
--- tools/bundlemaker/xmake.lua
+++ tools/bundlemaker/xmake.lua
@@ -1,6 +1,9 @@
 target("openminecraft-bundlemaker")
 set_kind("binary")
 add_files("**.cpp")
+if is_plat("windows") then
+    add_files("getopt.c")
+end
 add_deps("openminecraft-specs", "openminecraft-log", "openminecraft-binary", "openminecraft-plat")
 add_packages("fmt")
 add_includedirs("../../include")
