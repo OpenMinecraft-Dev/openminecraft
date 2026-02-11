30fc311e94df2fc67424845bc633fddf9ae6cbfc
MetsukiMio <guoxiuchen20170402@163.com>
2026-02-11 17:56:49 +0800

    [CI/CD] fix

--- xmake.lua
+++ xmake.lua
@@ -39,7 +39,6 @@ add_requires(
 	"opengl-headers",
 	{ system = false }
 )
-add_requires("libsdl3", { system = false, configs = { x11 = false } })
 add_requires("boost", { system = false, configs = { stacktrace = true, asio = true } })
 add_requires("fmt", { system = false, configs = { header_only = true } })
 add_requires("harfbuzz", { system = false, configs = { freetype = false } })
