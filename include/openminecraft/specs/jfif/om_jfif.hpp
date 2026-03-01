17e460e92e3c7016bbeaf4f4ac127d3092f26837
MetsukiMio <guoxiuchen20170402@163.com>
2026-03-01 18:06:42 +0800

    [Specs] jpg: test

--- include/openminecraft/specs/jfif/om_jfif.hpp
+++ include/openminecraft/specs/jfif/om_jfif.hpp
@@ -1,6 +1,7 @@
 #ifndef OM_JFIF_HPP
 #define OM_JFIF_HPP
 
+#include "openminecraft/log/om_log_common.hpp"
 #include "openminecraft/mem/om_mem_stl_allocator.hpp"
 #include <cstdint>
 #include <istream>
@@ -13,7 +14,12 @@ enum OMJfifState
 {
     None,
     TagBegin,
-    TagContentApp0
+    TagContentApp0,
+    TagContentApp2,
+    TagComment,
+    TagQuantizationTable,
+    TagStartOfFrame,
+    TagHuffmanTable
 };
 
 struct OMJfifThumbnailPixel
@@ -35,6 +41,43 @@ struct OMJfifApp0Header
     uint8_t thumbnailX, thumbnailY;
 };
 
+struct OMJfifApp2Header
+{
+    uint16_t length;
+};
+
+struct OMJfifQuantizationTable
+{
+    uint16_t length;
+    uint8_t destination;
+    uint8_t table[64];
+};
+
+struct OMJfifComponentStat
+{
+    uint8_t id;
+    uint8_t factor;
+    uint8_t tableId;
+};
+
+#pragma pack(1)
+struct OMJfifStartOfFrame
+{
+    uint16_t length;
+    uint8_t precision;
+    uint16_t height;
+    uint16_t width;
+    uint8_t components;
+};
+
+struct OMJfifHuffmanTable
+{
+    uint16_t length;
+    uint8_t info; // high 4 bit -> DC/AC, low 4 bit -> table id
+    uint8_t counts[16];
+};
+#pragma pack()
+
 class OMJfifFile
 {
   public:
@@ -42,12 +85,19 @@ class OMJfifFile
     ~OMJfifFile();
 
     void parse(std::shared_ptr<std::istream> input);
+    static uint16_t readLen(std::shared_ptr<std::istream> input);
 
   private:
     OMJfifState state = None;
 
     OMJfifApp0Header headerApp0;
+    OMJfifApp2Header headerApp2;
+    OMJfifStartOfFrame headerStartOfFrame;
+    std::vector<OMJfifComponentStat, mem::OMStlAllocator<allocatorTag, OMJfifComponentStat>> components;
     std::vector<OMJfifThumbnailPixel, mem::OMStlAllocator<allocatorTag, OMJfifThumbnailPixel>> thumbnail;
+    std::vector<OMJfifQuantizationTable, mem::OMStlAllocator<allocatorTag, OMJfifQuantizationTable>> quantizationTable;
+
+    log::OMLogger logger;
 };
 }; // namespace openminecraft::specs::jfif
 
--- src/specs/jfif/om_jfif.cpp
+++ src/specs/jfif/om_jfif.cpp
@@ -5,7 +5,7 @@
 
 namespace openminecraft::specs::jfif
 {
-OMJfifFile::OMJfifFile()
+OMJfifFile::OMJfifFile() : logger("OMJfifFile", this)
 {
 }
 OMJfifFile::~OMJfifFile()
@@ -42,6 +42,22 @@ void OMJfifFile::parse(std::shared_ptr<std::istream> input)
             case 0xe0:
                 state = TagContentApp0;
                 break;
+            case 0xe2:
+                state = TagContentApp2;
+                break;
+            case 0xfe:
+                state = TagComment;
+                break;
+            case 0xdb:
+                state = TagQuantizationTable;
+                break;
+            case 0xc0:
+
+                state = TagStartOfFrame;
+                break;
+            case 0xc4:
+                state = TagHuffmanTable;
+                break;
             default:
                 std::cout << "0x" << std::hex << (int)b << std::dec << std::endl;
                 state = None;
@@ -63,7 +79,73 @@ void OMJfifFile::parse(std::shared_ptr<std::istream> input)
             state = None;
             break;
         }
+
+        case TagContentApp2: {
+            input->read(reinterpret_cast<char *>(&headerApp2), sizeof(OMJfifApp2Header));
+            headerApp2.length = binary::be16ToNative(headerApp2.length);
+            input->ignore(headerApp2.length - 2);
+
+            state = None;
+            break;
+        }
+
+        case TagComment: {
+            auto length = readLen(input);
+            std::string buf;
+            buf.resize(length);
+            input->read(buf.data(), length - 2);
+            logger.info("JFIF Comment: {}", buf);
+            state = None;
+            break;
+        }
+
+        case TagQuantizationTable: {
+            OMJfifQuantizationTable tb;
+            input->read(reinterpret_cast<char *>(&tb), sizeof(OMJfifQuantizationTable));
+            quantizationTable.push_back(tb);
+            state = None;
+            break;
+        }
+
+        case TagStartOfFrame: {
+            input->read(reinterpret_cast<char *>(&headerStartOfFrame), sizeof(OMJfifStartOfFrame));
+            headerStartOfFrame.length = binary::be16ToNative(headerStartOfFrame.length);
+            headerStartOfFrame.width = binary::be16ToNative(headerStartOfFrame.width);
+            headerStartOfFrame.height = binary::be16ToNative(headerStartOfFrame.height);
+            for (int i = 0; i < headerStartOfFrame.components; i++)
+            {
+                OMJfifComponentStat st;
+                input->read(reinterpret_cast<char *>(&st), sizeof(OMJfifComponentStat));
+                components.push_back(st);
+            }
+
+            state = None;
+            break;
+        }
+
+        case TagHuffmanTable: {
+            OMJfifHuffmanTable tb;
+            input->read(reinterpret_cast<char *>(&tb), sizeof(OMJfifHuffmanTable));
+            tb.length = binary::be16ToNative(tb.length);
+
+            int cnt = 0;
+            for (int i = 0; i < 16; i++)
+            {
+                cnt += tb.counts[i];
+            }
+            logger.info("{} itts", cnt);
+            input->ignore(cnt);
+            state = None;
+            break;
+        }
         }
     }
+}
+
+uint16_t OMJfifFile::readLen(std::shared_ptr<std::istream> input)
+{
+    uint16_t l;
+    input->read(reinterpret_cast<char *>(&l), 2);
+    return binary::be16ToNative(l);
 }
 } // namespace openminecraft::specs::jfif
