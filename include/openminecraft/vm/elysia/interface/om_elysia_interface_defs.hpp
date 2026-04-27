776d799132370e7299d77284f84c9e167c423af3
MetsukiMio <guoxiuchen20170402@163.com>
2026-04-27 20:13:41 +0800

    [VM] elysia: fix

--- include/openminecraft/vm/elysia/interface/om_elysia_interface_defs.hpp
+++ include/openminecraft/vm/elysia/interface/om_elysia_interface_defs.hpp
@@ -12,11 +12,29 @@ class OMElysiaOop;
 class OMElysiaMethod;
 class OMElysiaField;
 
+struct OMElysiaNativeHandle
+{
+    OMElysiaOop *object;
+};
+
 struct OMElysiaNativeMethod
 {
     char *name;
     char *signature;
     void *funcPtr;
+};
+
+union OMElysiaNativeValue
+{
+    jboolean z;
+    jbyte b;
+    jshort s;
+    jchar c;
+    jint i;
+    jfloat f;
+    jlong j;
+    jdouble d;
+    OMElysiaNativeHandle *l;
 };
 
 struct OMElysiaNativeInterface;
@@ -30,17 +48,19 @@ struct OMElysiaNativeInterface
     void *reserve3;
 
     jint (*GetVersion)(OMElysiaJNIEnv *env);
-    OMElysiaKlass *(*DefineClass)(OMElysiaJNIEnv *env, const char *name, OMElysiaOop *loader, const jbyte *buf);
+    OMElysiaKlass *(*DefineClass)(OMElysiaJNIEnv *env, const char *name, OMElysiaNativeHandle *loader, const jbyte *buf);
     OMElysiaKlass *(*FindClass)(OMElysiaJNIEnv *env, const char *name);
 
-    OMElysiaMethod *(*FromReflectedMethod)(OMElysiaJNIEnv *env, OMElysiaOop *method);
-    OMElysiaField *(*FromReflectedField)(OMElysiaJNIEnv *env, OMElysiaOop *field);
+    OMElysiaMethod *(*FromReflectedMethod)(OMElysiaJNIEnv *env, OMElysiaNativeHandle *method);
+    OMElysiaField *(*FromReflectedField)(OMElysiaJNIEnv *env, OMElysiaNativeHandle *field);
 
     OMElysiaOop *(*ToReflectedMethod)(OMElysiaJNIEnv *env, OMElysiaKlass *klass, OMElysiaMethod *method,
                                       jboolean isStatic);
 
     OMElysiaKlass *(*GetSuperclass)(OMElysiaJNIEnv *env, OMElysiaKlass *klass);
     jboolean (*IsAssignableFrom)(OMElysiaJNIEnv *env, OMElysiaKlass *sub, OMElysiaKlass *sup);
+
+    OMElysiaNativeHandle *(*ToReflectedField)(OMElysiaJNIEnv *env, OMElysiaKlass *klass, OMElysiaField *field, jboolean isStatic);
 };
 } // namespace openminecraft::vm::elysia
 
