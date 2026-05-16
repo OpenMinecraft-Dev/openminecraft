7c3df6b947c660a3bcea1ae47c3d805407c5a151
MetsukiMio <guoxiuchen20170402@163.com>
2026-05-16 13:34:00 +0800

    [General] new plat config

--- utils.lua
+++ utils.lua
@@ -12,7 +12,7 @@ end
 
 -- platform-dependent & arch-dependent source config
 -- platform: plat-$(platform_name)
--- arch: arch-%(arch_name)
+-- arch: arch-$(arch_name)
 function addExtFiles(config)
 	if config == nil then
 		return
@@ -35,46 +35,27 @@ function addExtFiles(config)
 	addExtFilesSub(is_plat("harmony"), "plat", "harmony")
 	addExtFilesSub(not mobile(), "plat", "desktop")
 
-	if is_arch("x86", "i386", "x86_64", "x64") then
-		if not is_plat("windows", "mingw") then
-			add_files("arch/x86/unix_**.S")
-		else
-			add_files("arch/x86/msvc_**.S")
+	function addExtFilesSub2(cond, type, name, ismsvc)
+		if cond and config[type .. "-" .. name] then
+			if is_plat("windows", "mingw") and ismsvc then
+				add_files(type .. "/" .. name .. "/msvc_**.asm")
+			else
+				add_files(type .. "/" .. name .. "/unix_**.S")
+			end
+			add_files(type .. "/" .. name .. "/**.cpp")
+
+			print("Added subdirectory " .. type .. "/" .. name)
 		end
-		add_files("arch/x86/**.cpp")
-	elseif is_arch("arm64-v8a", "arm64") then
-		if not is_plat("windows", "mingw") then
-			add_files("arch/aarch64/unix_**.S")
-		else
-			add_files("arch/aarch64/msvc_**.S")
-		end
-		add_files("arch/aarch64/**.cpp")
-	elseif is_arch("armeabi", "armv7k", "armeabi-v7a", "arm", "armv7s", "armv7") then
-		add_files("arch/arm/unix_**.S")
-		add_files("arch/arm/**.cpp")
-	elseif is_arch("loong64") then
-		add_files("arch/loongarch/unix_**.S")
-		add_files("arch/loongarch/**.cpp")
-	elseif is_arch("mips64", "mips64el", "mip64") then
-		add_files("arch/mips64/unix_**.S")
-		add_files("arch/mips64/**.cpp")
-	elseif is_arch("mips", "mipsel") then
-		add_files("arch/mips/unix_**.S")
-		add_files("arch/mips/**.cpp")
-	elseif is_arch("ppc") then
-		add_files("arch/ppc/unix_**.S")
-		add_files("arch/ppc/**.cpp")
-	elseif is_arch("ppc64") then
-		add_files("arch/ppc64/unix_**.S")
-		add_files("arch/ppc64/**.cpp")
-	elseif is_arch("riscv") then
-		add_files("arch/riscv/unix_**.S")
-		add_files("arch/riscv/**.cpp")
-	elseif is_arch("riscv64") then
-		add_files("arch/riscv64/unix_**.S")
-		add_files("arch/riscv64/**.cpp")
-	else
-		add_files("arch/fallback/unix_**.S")
-		add_files("arch/fallback/**.cpp")
 	end
+
+	addExtFilesSub2(is_arch("x86", "i386", "x86_64", "x86"), "arch", "x86", true)
+	addExtFilesSub2(is_arch("arm64-v8a", "arm64"), "arch", "aarch64", true)
+	addExtFilesSub2(is_arch("armeabi", "armv7k", "armeabi-v7a", "arm", "armv7s", "armv7"), "arch", "arm", false)
+	addExtFilesSub2(is_arch("loong64"), "arch", "loongarch", false)
+	addExtFilesSub2(is_arch("mips64", "mips64el", "mip64"), "arch", "mips64", false)
+	addExtFilesSub2(is_arch("mips", "mipsel"), "arch", "mips", false)
+	addExtFilesSub2(is_arch("ppc"), "arch", "ppc", false)
+	addExtFilesSub2(is_arch("ppc64"), "arch", "ppc64", false)
+	addExtFilesSub2(is_arch("riscv"), "arch", "riscv", false)
+	addExtFilesSub2(is_arch("riscv64"), "arch", "riscv64", false)
 end
