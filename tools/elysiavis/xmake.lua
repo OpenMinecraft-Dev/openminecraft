85754ff381eeb1701afd646a7d2fa91c97d89d4c
MetsukiMio <guoxiuchen20170402@163.com>
2026-05-29 19:43:36 +0800

    [VM] elysia: new visual

--- tools/elysiavis/xmake.lua
+++ tools/elysiavis/xmake.lua
@@ -1,8 +1,6 @@
-add_requires("ftxui", { system = false })
-
 target("openminecraft-elysiavis")
 set_kind("binary")
 add_files("**.cpp")
 add_deps("openminecraft-log", "openminecraft-vm", "openminecraft-mem")
-add_packages("fmt", "ftxui")
+add_packages("fmt")
 add_includedirs("../../include")
