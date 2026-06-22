611f951a6e20751a3adcc22bd451ad52adbb2e21
MetsukiMio <guoxiuchen20170402@163.com>
2026-06-23 05:54:32 +0800

    [General] find libffi

--- extlibs/libffi.lua
+++ extlibs/libffi.lua
@@ -5,10 +5,15 @@ package("libffi")
 
     on_fetch(function (package, opt)
         local sysroot = get_config("sdk") or "/home/coder2/Downloads/crosstoolchain/sysroot"
-        local arch = "riscv64-linux-gnu"  -- 根据你的目标架构调整
+	local arch = get_config("arch")
+	local mapping = {
+            riscv64 = "riscv64-linux-gnu",
+            s390x = "s390x-linux-gnu",
+	    ppc64 = "powerpc64le-linux-gnu"
+	}
         return {
             includedirs = path.join(sysroot, "usr/include"),
-            linkdirs = path.join(sysroot, "usr/lib", arch),
+            linkdirs = path.join(sysroot, "usr/lib", mapping[arch]),
             links = "ffi"
         }
     end)
