4f259ca28c6ffded40cc7f96ad6139f520c4c290
MetsukiMio <guoxiuchen20170402@163.com>
2026-06-28 09:55:11 +0800

    [VM] elysium: add getname api (apple)

--- src/vm/plat/unix/elysia/om_elysia_threadmodel_plat.cpp
+++ src/vm/plat/unix/elysia/om_elysia_threadmodel_plat.cpp
@@ -1,10 +1,6 @@
 #include "openminecraft/vm/elysia/om_elysia_threadmodel.hpp"
 #include <pthread.h>
 
-#ifdef OM_PLATFORM_ANDROID
-int pthread_getname_np(pthread_t, char *, int);
-#endif
-
 namespace openminecraft::vm::elysia
 {
 std::string OMElysiaThread::getName()
@@ -14,9 +10,7 @@ std::string OMElysiaThread::getName()
 void OMElysiaThread::setName(std::string n)
 {
     threadName = n;
-#ifdef OM_PLATFORM_APPLE
-    thread_set_name(pthread_mach_thread_np((pthread_t)nativeHandle), n.c_str());
-#else
+#ifndef OM_PLATFORM_APPLE
     pthread_setname_np((pthread_t)nativeHandle, n.c_str());
 #endif
 }
