9cd86f0be920008fe86d2c2baf30c08520455767
Coder2 <83895170+MetsukiMio@users.noreply.github.com>
2026-07-17 09:14:55 +0800

    更新 updateassets.sh

--- scripts/updateassets.sh
+++ scripts/updateassets.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 xmake run -w . openminecraft-bundlemaker -a Cyrene -c -t boot.bundle -P bootassets
 xmake run -w . openminecraft-bundlemaker -r -t boot.bundle
+pwd
+ls
 # xxd -i res.bundle > include/openminecraft/resource/bootassets.h
 # rm res.bundle
