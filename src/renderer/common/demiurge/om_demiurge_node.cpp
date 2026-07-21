33496bf62a586f7551fee61718e42db9e7d24c0f
MetsukiMio <guoxiuchen20170402@163.com>
2026-07-21 11:24:21 +0800

    [Renderer] demiurge: yoga lib

--- src/renderer/common/demiurge/om_demiurge_node.cpp
+++ src/renderer/common/demiurge/om_demiurge_node.cpp
@@ -4,6 +4,30 @@
 
 namespace openminecraft::renderer::common::demiurge
 {
+static void setSizeToYoga(YGNodeRef node, OMDemiurgeSize size, bool isWidth)
+{
+    switch (size.unit)
+    {
+    case OMDemiurgeSize::Pixel:
+        if (isWidth)
+            YGNodeStyleSetWidth(node, size.value);
+        else
+            YGNodeStyleSetHeight(node, size.value);
+        break;
+    case OMDemiurgeSize::Percent:
+        if (isWidth)
+            YGNodeStyleSetWidthPercent(node, size.value);
+        else
+            YGNodeStyleSetHeightPercent(node, size.value);
+        break;
+    case OMDemiurgeSize::Fit:
+        if (isWidth)
+            YGNodeStyleSetWidthAuto(node);
+        else
+            YGNodeStyleSetHeightAuto(node);
+        break;
+    }
+}
 OMDemiurgeNode::OMDemiurgeNode()
 {
     yogaNode = YGNodeNew();
@@ -17,10 +41,17 @@ OMDemiurgeNode::~OMDemiurgeNode()
 }
 void OMDemiurgeNode::syncStyle()
 {
+    setSizeToYoga(yogaNode, style.width, true);
+    setSizeToYoga(yogaNode, style.height, false);
     YGNodeStyleSetMinWidth(yogaNode, style.minWidth);
     YGNodeStyleSetMinHeight(yogaNode, style.minHeight);
     YGNodeStyleSetMaxWidth(yogaNode, style.maxWidth);
     YGNodeStyleSetMaxHeight(yogaNode, style.maxHeight);
+
+#define ygsync(ygname, myname) YGNodeStyleSet##ygname(yogaNode, YGEdgeTop, style.myname.top);
+    ygsync(Padding, padding);
+    ygsync(Margin, margin);
+    ygsync(Border, border);
 }
 void OMDemiurgeNode::layout()
 {
