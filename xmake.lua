3c8e8c5912f44dc2e52b394eb36d501f65917754
MetsukiMio <guoxiuchen20170402@163.com>
2026-07-25 18:26:06 +0800

    [Renderer] demiurge: remove vma

--- xmake.lua
+++ xmake.lua
@@ -114,7 +114,6 @@ add_requires(
 	"c-ares",
 	"libsdl3",
 	"yoga",
-	"vulkan-memory-allocator",
 	{ system = false }
 )
 add_requires("boost", { system = false, configs = { stacktrace = true, asio = true } })
