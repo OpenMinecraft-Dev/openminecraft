39c61af84a55c3a6b5e8f2cfeb542a12ee3c1c80
MetsukiMio <guoxiuchen20170402@163.com>
2026-08-08 12:59:50 +0800

    [Renderer] opengl: change

--- bootassets/openminecraft-renderer/shaders/demiurge/rect.frag.glsl
+++ bootassets/openminecraft-renderer/shaders/demiurge/rect.frag.glsl
@@ -1,9 +1,9 @@
 #version 330 core
 #extension GL_ARB_separate_shader_objects : enable
 
-layout(location = 0) in vec4 inColor;
-layout(location = 0) out vec4 result;
+layout(location = 0) in vec4 rectColor;
+layout(location = 0) out vec4 outColor;
 
 void main() {
-    result = inColor;
+    outColor = rectColor;
 }
--- bootassets/openminecraft-renderer/shaders/demiurge/rect.vert.glsl
+++ bootassets/openminecraft-renderer/shaders/demiurge/rect.vert.glsl
@@ -8,7 +8,7 @@ layout(location = 1) in vec4 inRectPos;
 layout(location = 2) in vec4 inRectColor;
 layout(location = 3) in float inRectDepth;
 
-layout(location = 0) out vec4 outColor;
+layout(location = 0) out vec4 rectColor;
 
 uniform ScreenData {
     float width;
@@ -19,5 +19,5 @@ void main() {
     vec2 screenPos = inRectPos.xy + inPosition.xy * inRectPos.zw;
 
     gl_Position = vec4(geom_toNdc(screenPos, ubo.width, ubo.height), inRectDepth, 1.0);
-    outColor = inRectColor;
+    rectColor = inRectColor;
 }
