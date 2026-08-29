b6e394769ff99d46cb07e8b1d8de286a62b4eef5
MetsukiMio <guoxiuchen20170402@163.com>
2026-08-29 15:35:48 +0800

    [Network] unix impl

--- src/openminecraft-core/network/xmake.lua
+++ src/openminecraft-core/network/xmake.lua
@@ -3,9 +3,12 @@ local config = {
 	["plat-windows"] = true,
 }
 
+includes(path.join(os.projectdir(), "utils.lua"))
+
 target("openminecraft-network")
 set_kind("static")
 add_packages("fmt", "boost")
 add_deps("openminecraft-log")
-add_files("**.cpp")
+add_files("**.cpp|plat/**")
+addExtFiles(config)
 add_includedirs(path.join(os.projectdir(), "include"))
